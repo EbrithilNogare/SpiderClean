85582d011e4adcdc0b48ecad83a1c8f055245154
David Napravnik <ebrithilnogare@gmail.com>
2025-03-29 11:31:10 +0100

    basic movement

--- Assets/Scripts/CleanerController.cs
+++ Assets/Scripts/CleanerController.cs
@@ -6,57 +6,76 @@ public class CleanerController : MonoBehaviour
     public float radius;
     public float speed;
     public float climbingSpeed;
+    public float minRadius = 1f;
+    public float maxRadius = 10f;
 
     public Transform craneL;
     public Transform craneR;
     public LineRenderer lineRenderer;
 
-    private bool[] playerInput = new bool[2]; //0==up, 1==down
+    private bool[] playerInput = new bool[2]; // 0 == up, 1 == down
     private bool usingCraneL = false;
-
-    void Start()
-    {
-
-    }
+    private float angle = Mathf.PI / 2f;
+    private float swingDirection = 1f;
 
     void Update()
     {
-        DrawSpiderWeb();
         Climbing();
+        Swinging();
+        DrawSpiderWeb();
+    }
 
+    private void Swinging()
+    {
         Transform crane = usingCraneL ? craneL : craneR;
-        // Swinging cleaner under the crane
+        angle += speed * swingDirection * Time.deltaTime;
+        angle = Mathf.Clamp(angle, 0, Mathf.PI);
 
+        float x = crane.position.x + Mathf.Cos(angle) * radius;
+        float y = crane.position.y - Mathf.Sin(angle) * radius;
+        transform.position = new Vector3(x, y, transform.position.z);
 
-
+        if (angle >= Mathf.PI || angle <= 0)
+            swingDirection *= -1f;
     }
 
     public void Left(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && !usingCraneL)
+        {
             usingCraneL = true;
+            SwitchCrane();
+        }
     }
 
     public void Right(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && usingCraneL)
+        {
             usingCraneL = false;
+            SwitchCrane();
+        }
+    }
+
+    private void SwitchCrane()
+    {
+        Transform newCrane = usingCraneL ? craneL : craneR;
+        Vector2 offset = transform.position - newCrane.position;
+        radius = offset.magnitude;
+        angle = Mathf.Atan2(-offset.y, offset.x);
+        swingDirection = Mathf.Sign(Mathf.Sin(angle));
     }
 
     public void Up(InputAction.CallbackContext context)
     {
-        if (context.started)
-            playerInput[0] = true;
-        if (context.canceled)
-            playerInput[0] = false;
+        if (context.started) playerInput[0] = true;
+        if (context.canceled) playerInput[0] = false;
     }
 
     public void Down(InputAction.CallbackContext context)
     {
-        if (context.started)
-            playerInput[1] = true;
-        if (context.canceled)
-            playerInput[1] = false;
+        if (context.started) playerInput[1] = true;
+        if (context.canceled) playerInput[1] = false;
     }
 
     private void DrawSpiderWeb()
@@ -67,14 +86,7 @@ public class CleanerController : MonoBehaviour
 
     private void Climbing()
     {
-        if (playerInput[0]) // up
-        {
-            radius -= climbingSpeed * Time.deltaTime;
-        }
-
-        if (playerInput[1]) // down
-        {
-            radius += climbingSpeed * Time.deltaTime;
-        }
+        if (playerInput[0]) radius = Mathf.Max(minRadius, radius - climbingSpeed * Time.deltaTime);
+        if (playerInput[1]) radius = Mathf.Min(maxRadius, radius + climbingSpeed * Time.deltaTime);
     }
 }
