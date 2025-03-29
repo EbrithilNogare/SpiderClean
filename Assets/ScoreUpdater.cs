fdf7f2c548c5f277bedede16607b35fbca5bf6cf
David Napravnik <ebrithilnogare@gmail.com>
2025-03-29 12:47:54 +0100

    fixed foreground

--- Assets/ScoreUpdater.cs
+++ Assets/ScoreUpdater.cs
@@ -4,9 +4,11 @@ public class ScoreUpdater : MonoBehaviour
 {
     public TMPro.TextMeshProUGUI scoreText;
     public DirtController dirtController;
+    public string beforeText = "Remaining dirt: ";
+    public string afterText = " %";
 
     void Update()
     {
-        scoreText.text = (int)(100 - (float)dirtController.dirtCount / (float)dirtController.startingDirtCount * 100f) + "%";
+        scoreText.text = beforeText + (int)((float)dirtController.dirtCount / (float)dirtController.startingDirtCount * 100f) + afterText;
     }
 }
